e667823c1c70aa682f33fe032626546c37835d62
MohammadSameer08 <60319674+MohammadSameer08@users.noreply.github.com>
2020-09-11 12:44:22 +0530

    Update q2.java

--- q2.java
+++ q2.java
@@ -15,9 +15,9 @@ public static int series(int n)
 	public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
-     int r;
-     int sum=0;
-     int n=sc.nextInt();
+     
+      int sum=0,n,r;
+      n=sc.nextInt();
      for(int i=0;i<n;i++)
      {
     	 r=series(i);
