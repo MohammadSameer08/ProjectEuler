3a5830664baf61064cd3c90e3c4079427ed4d9fa
MohammadSameer08 <60319674+MohammadSameer08@users.noreply.github.com>
2020-09-11 12:57:17 +0530

    Update q6.java

--- q6.java
+++ q6.java
@@ -1,23 +1,19 @@
-import java.util.*;
 public class q6 
 {
    public static void main(String args[])
    {
-	   Scanner sc=new Scanner(System.in);
-	   int sum1=0;
-    	int sum2=0;
-    	int n=100;
+	    int sum1=0, sum2=0,n=100;
 	   for(int i=1;i<=n;i++)
 	   {
-	                    sum1=sum1+(int)Math.pow(i,2);	                                             
-		                sum2=sum2+i;                          
+	                    sum1+=(int)Math.pow(i,2);	                                             
+		                sum2+=i;                          
 	   }
 	   sum2=(int) Math.pow(sum2,2);
    	
 	
-	System.out.println(sum1);
-	  System.out.println(sum2);
-	  System.out.println("Diff="+(sum2-sum1));
+    	System.out.println(sum1);
+	    System.out.println(sum2);
+	    System.out.println("Diff="+(sum2-sum1));
    }
  
 }
