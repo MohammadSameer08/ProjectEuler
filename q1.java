caea2da5788c8ad44e20edce63afb626018595a0
MohammadSameer08 <60319674+MohammadSameer08@users.noreply.github.com>
2020-09-11 12:43:15 +0530

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,3 +1,22 @@
+public class q1
+{
+
+	public static void main(String[] args)
+	{
+	int sum=0;
+	for(int i=1;i<1000;i++)
+	{
+		if(i%3==0||i%5==0)
+		{
+			sum+=i;
+		}
+	}
+	System.out.println(sum);
+	
+	}
+	
+
+}
 import java.util.*;
 public class q1
 {
